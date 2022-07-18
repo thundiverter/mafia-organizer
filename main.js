92b5bc331153a5acda3c84a48b95cc3572beb3f2
Ivan <43518470+Thundiverter@users.noreply.github.com>
2022-07-18 18:59:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,130 +3,94 @@ const { createApp } = Vue;
 createApp({
  data() {
   return {
-   roles: [
-    ['Мирный житель', 'Выбран голосованием', '', 'мирных жителей'],
-    ['Мафия', 'Мафия убила', 'kill', 'мафий'],
-    ['Комиссар', 'Комиссар проверил', 'check', 'комиссаров'],
-    ['Доктор', 'Доктор вылечил', 'heal', 'докторов']
+   currentTab: 0,
+   roles: roles,
+   usedRoles: [],
+   svg: svg,
+   time: {day: 'День', night: 'Ночь'},
+   players: [
+    {name: '', roleid: 0, dead: false}
    ],
-   users: [
-    {index: 0, name: '', role: 0, dead: false}
-   ],
-   turns: [],
-   players: [],
-   isPlaying: false,
+   playersAll: this.players,
    currentTurn: 0,
-   lastPlayerIndex: 0,
-   stat: {
-    killed: [],
-    healed: [],
-    checked: [],
-    killhealed: [],
-    voteed: []
-   },
-   gameEnd: ['', '']
+   turns: [
+    {time: 'night', kill: -1, heal: -1, check: -1, don: -1, maniac: -1}
+   ]
   }
  },
  methods: {
-  updatePlayers() {
-   const rows = document.querySelectorAll('#playerRow');
-   this.users = [];
-   for (row of rows) {
-    const rname = row.querySelector('input');
-    const rrole = row.querySelector('select');
-    this.users.push({name: rname.value, role: rrole.value, dead: false});
-   }
+  createNewPlayer() {
+   this.players.push(
+    {name: '', roleid: 0, dead: false}
+   )
   },
-  addPlayer() {
-   this.updatePlayers();
-   this.lastPlayerIndex += 1;
-   this.users.push({name: '', role: 0, dead: false});
-  },
+  
   removePlayer(id) {
-   this.updatePlayers();
-   this.users.splice(id, 1);
+   this.players.splice(id, 1);
   },
-  startGame() {
-   this.updatePlayers();
-   this.players = this.users;
-   this.isPlaying = true;
-   this.startNewTurn();
-  },
-  stats(s) {
-  this.stat[s+'ed'].push(this.users[this.turns[1][s]].name);
-  },
+  
   startNewTurn() {
+   const lturn = this.turns[0];
+   //console.log(lturn.vote)
   
-  const t = this.turns[0];
-   if (this.currentTurn < 1 || (this.currentTurn >= 1 && t.kill != -1 && t.vote != -1)) {
+   if (this.currentTurn % 2 == 1) {
+    if (lturn.vote != -1) {
+     this.players[lturn.vote].dead = true;
+    }
     
-   // new turn
-  
-   this.turns.unshift(
-     {kill: -1, check: -1, heal: -1, vote: -1}
-   );
-  
+    this.turns.unshift(
+     {time: 'night', kill: -1, heal: -1, check: -1, don: -1, maniac: -1}
+    )
+   }
+   else {
+    if (lturn.kill != -1 && lturn.kill != lturn.heal) {
+     this.players[lturn.kill].dead = true;
+    }
    
+    this.turns.unshift(
+     {time: 'day', vote: -1}
+    )
+   }
    this.currentTurn += 1;
    
-   if (this.currentTurn > 1) {
-    const lastTurn = this.turns[1];
+   //this.playersAll = this.players;
+  },
+  
+  getTurn(turnid, turn) {
+   if (turn.time == 'night') {
+    return (this.turns.length - turnid+1)/2;
+   }
+   else {
+    return (this.turns.length - turnid) / 2;
+   }
    
-    
-    // player is killed
-    if (lastTurn.kill != lastTurn.heal) {
-     this.users[lastTurn.kill].dead = true;
-     this.stats('kill');
-    }
-    // player is killhealed
-    if (lastTurn.kill == lastTurn.heal) {
-     lastTurn.killheal = lastTurn.heal;
-     this.stats('killheal');
-    }
-    
-    // player is kicked
-    if (lastTurn.vote >= 0) {
-     this.users[lastTurn.vote].dead = true;
-     this.stats('vote');
-    }
-    
-    // other stats
-    if (lastTurn.heal >= 0) { this.stats('heal'); }
-    if (lastTurn.check >= 0) { this.stats('check'); }
-    
-    }
-   }
+   return ( (this.currentTurn - this.currentTurn%2)/2 - (turnid - turnid%2)/2 ) + 1;
   },
-  isCurrentTurn(turnid) {
-   return this.currentTurn == this.turns.length - turnid;
-  },
-  changeTurnInfo (e, turnid, act) {
-   this.turns[turnid][act] = e.target.value;
-  },
-  amountof(r) {
-   let count = 0;
-   for (user of this.users) {
-    if (user.role === r) {
-     count += 1;
-    }
-   }
-   return count + ' ' + this.roles[r][3];
+  
+  getStatistics(turn) {
+   return 0;
   }
  },
  computed: {
- // Returns a list of players who are still alive
-  playersAlive: function() {
+  isPlaying() {
+   flag = true;
+   for (player of this.players) {
+    if (player.name === '') {
+     flag = false;
+     break;
+    }
+   }
+   return flag;
+  },
+  playersAlive() {
    let res = [];
-   const p = this.users;
-   for (i in p) {
-    p[i]["index"] = i;
-    //p[i]["name"] += i;
-    if (!p[i].dead) {
-     res.push(p[i]);
+   for (i in this.players) {
+    this.players[i]['id'] = i;
+    if (!this.players[i].dead) {
+     res.push(this.players[i]);
     }
    }
    return res;
-   //return this.players.filter(i => !i.dead);
   }
  }
 }).mount('#app');
